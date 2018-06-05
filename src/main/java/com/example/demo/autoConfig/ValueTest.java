de607e2ac607225b418611add0ca5420dc2d72ec
malili <2381725611@qq.com>
2018-06-05 16:43:57 +0800

    @Value自动配置

---
+++ src/main/java/com/example/demo/autoConfig/ValueTest.java
@@ -0,0 +1,27 @@
+package com.example.demo.autoConfig;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @program:
+ * @description:  这里直接用@Value 就直接自动配置了
+ * @author: malili
+ * @create: 2018-06-05 14:37
+ **/
+@RestController
+public class ValueTest {
+    @Value("${valueTest.url.testHello}")
+    private String testHello;
+    @Value("${valueTest.url.testMe}")
+    private String testMe;
+
+    @RequestMapping(value = "index")
+    public String sayHello(){
+        System.err.println("我今天和一个帅哥say"+testHello);
+        return "我今天和一个帅哥say"+testHello;
+    }
+
+
+}
